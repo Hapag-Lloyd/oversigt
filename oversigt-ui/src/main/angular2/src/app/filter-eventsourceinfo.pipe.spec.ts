fb12283b7017f73dc22b64ed8e52a1cff763c439
Olaf Neumann <github@olafneumann.org>
2018-11-01 21:50:35 +0100

    remove tests

--- oversigt-ui/src/main/angular2/src/app/filter-eventsourceinfo.pipe.spec.ts
+++
@@ -1,8 +0,0 @@
-import { FilterEventsourceinfoPipe } from './filter-eventsourceinfo.pipe';
-
-describe('FilterEventsourceinfoPipe', () => {
-  it('create an instance', () => {
-    const pipe = new FilterEventsourceinfoPipe();
-    expect(pipe).toBeTruthy();
-  });
-});
