01e07667e1a9074feec288ebfc66d9689b81c332
Olaf Neumann <github@olafneumann.org>
2019-01-14 23:35:00 +0100

    clean up

--- oversigt-ui/src/main/angular2/src/app/app.module.ts
+++ oversigt-ui/src/main/angular2/src/app/app.module.ts
@@ -4,13 +4,22 @@ import { HttpClientModule } from '@angular/common/http';
 import { RouterModule, Routes } from '@angular/router';
 import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { ToastrModule } from 'ngx-toastr';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+
+import { ClarityModule, ClrFormsNextModule } from '@clr/angular';
+import { environment } from 'src/environments/environment';
 
 import { AppComponent } from './app.component';
 import { DashboardService, ApiModule, Configuration } from '../oversigt-client';
+import { JsonSchemaEditorModule } from './json-schema-editor/json-schema-editor.module';
 import { WelcomeComponent } from './welcome/welcome.component';
 import { DashboardComponent } from './dashboard/dashboard.component';
+import { HeaderComponent } from './config/_header/header.component';
 import { LoginComponent } from './login/login.component';
+import { SimpleEditorComponent } from './config/_simple-editor/simple-editor.component';
+import { EventsourceButtonComponent } from './eventsource-button/eventsource-button.component';
 import { ConfigurationComponent } from './config/main/main.component';
+import { ConfigListChildcomponentsComponent } from './config/list-childcomponents/config-list-childcomponents.component';
 import { ConfigPropertyComponent } from './config/property/config-property.component';
 import { ConfigSystemComponent } from './config/system/config-system.component';
 import { ConfigServerComponent } from './config/system-server/config-server.component';
@@ -19,27 +28,19 @@ import { ConfigLogsLogfileComponent } from './config/system-logfiles/config-logs
 import { ConfigLogsLoggerComponent } from './config/system-loggers/config-logs-logger.component';
 import { ConfigEventsComponent } from './config/system-events/config-events.component';
 import { ConfigEventsourcesComponent } from './config/eventsources-main/config-eventsources.component';
-import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-import { SimpleEditorComponent } from './config/_simple-editor/simple-editor.component';
+import { ConfigEventsourcesListComponent } from './config/eventsources-list/config-eventsources-list.component';
 import { ConfigEventsourceCreateComponent } from './config/eventsource-create/config-eventsource-create.component';
+import { ConfigEventsourcesDetailsComponent } from './config/eventsources-details/config-eventsources-details.component';
 import { ConfigEventsourceEditorComponent } from './config/eventsource-editor/config-eventsource-editor.component';
 import { FilterEventsourceinfoPipe } from './pipes/filter-eventsourceinfo.pipe';
-import { HeaderComponent } from './config/_header/header.component';
-import { PrettyPrintPipe } from './pipes/prettyprint.pipe';
-import { FilterEventitemPipe } from './pipes/filter-eventitem.pipe';
 import { ConfigDashboardsComponent } from './config/dashboards/config-dashboards.component';
 import { ConfigDashboardsEditComponent } from './config/dashboards-edit/config-dashboards-edit.component';
-import { EventsourceButtonComponent } from './eventsource-button/eventsource-button.component';
-import { ConfigEventsourcesDetailsComponent } from './config/eventsources-details/config-eventsources-details.component';
-import { FilterForRolePipe } from './pipes/filter-for-role.pipe';
-import { environment } from 'src/environments/environment';
-import { AuthGuard } from './guards/auth.guard';
-import { JsonSchemaEditorModule } from './json-schema-editor/json-schema-editor.module';
-import { ClarityModule, ClrFormsNextModule } from '@clr/angular';
-import { ConfigEventsourcesListComponent } from './config/eventsources-list/config-eventsources-list.component';
 import { ConfigDashboardWidgetComponent } from './config/dashboards-widget/config-dashboards-widget.component';
 import { ConfigDashboardWidgetAddComponent } from './config/dashboards-widget-add/config-dashboards-widget-add.component';
-import { ConfigListChildcomponentsComponent } from './config/list-childcomponents/config-list-childcomponents.component';
+import { AuthGuard } from './guards/auth.guard';
+import { PrettyPrintPipe } from './pipes/prettyprint.pipe';
+import { FilterEventitemPipe } from './pipes/filter-eventitem.pipe';
+import { FilterForRolePipe } from './pipes/filter-for-role.pipe';
 import { HasRoleDirective } from './directives/role-based.directive';
 
 const appRoutes: Routes = [
@@ -65,7 +66,7 @@ const appRoutes: Routes = [
       { path: 'loggers',              component: ConfigLogsLoggerComponent },
       { path: 'events',               component: ConfigEventsComponent },
       { path: 'threads',              component: ConfigThreadsComponent },
-      { path: 'server',               component: ConfigServerComponent }, // TODO
+      { path: 'server',               component: ConfigServerComponent },
     ]},
     { path: 'properties',             component: ConfigListChildcomponentsComponent },
     { path: 'properties/:name',       component: ConfigPropertyComponent },
