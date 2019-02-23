e945fbe5f30aca193f11da6dbfefbe9c26040e96
Olaf Neumann <github@olafneumann.org>
2019-02-23 14:00:36 +0100

    fix role lookup

--- core/src/main/java/com/hlag/oversigt/security/RoleProvider.java
+++ core/src/main/java/com/hlag/oversigt/security/RoleProvider.java
@@ -1,14 +1,17 @@
 package com.hlag.oversigt.security;
 
+import static java.util.stream.Collectors.toSet;
+
 import java.util.Collection;
 import java.util.HashSet;
 import java.util.List;
 import java.util.Set;
-import java.util.stream.Collectors;
+import java.util.stream.Stream;
 
 import com.google.inject.Inject;
 import com.google.inject.Singleton;
 import com.google.inject.name.Named;
+import com.hlag.oversigt.model.DashboardController;
 import com.hlag.oversigt.storage.Storage;
 
 @Singleton
@@ -18,35 +21,38 @@ public class RoleProvider {
 	private List<String> adminUserIds;
 	@Inject
 	private Storage storage;
+	@Inject
+	private DashboardController controller;
 
 	public Set<Role> getRoles(String userid) {
 		Set<Role> roles = new HashSet<>();
 
 		if (adminUserIds.contains(userid.toLowerCase().trim())) {
 			roles.add(Role.SERVER_ADMIN);
+			addWithDefaultRole(roles,
+					controller.getDashboardIds().stream().map(Role::getDashboardOwnerRole),
+					Role.DASHBOARD_OWNER);
+			addWithDefaultRole(roles,
+					controller.getDashboardIds().stream().map(Role::getDashboardEditorRole),
+					Role.DASHBOARD_EDITOR);
+		} else {
+			addWithDefaultRole(roles,
+					storage.getOwnedDashboardIds(userid).stream().map(Role::getDashboardOwnerRole),
+					Role.DASHBOARD_OWNER);
+			addWithDefaultRole(roles,
+					storage.getOwnedDashboardIds(userid).stream().map(Role::getDashboardEditorRole),
+					Role.DASHBOARD_EDITOR);
+
+			addWithDefaultRole(roles,
+					storage.getEditableDashboardIds(userid).stream().map(Role::getDashboardEditorRole),
+					Role.DASHBOARD_EDITOR);
 		}
 
-		addWithDefaultRole(roles,
-				storage.getOwnedDashboardIds(userid)
-						.stream()
-						.map(Role::getDashboardOwnerRole)
-						.collect(Collectors.toSet()),
-				Role.DASHBOARD_OWNER);
-		addWithDefaultRole(roles,
-				storage.getOwnedDashboardIds(userid)
-						.stream()
-						.map(Role::getDashboardEditorRole)
-						.collect(Collectors.toSet()),
-				Role.DASHBOARD_EDITOR);
-
-		addWithDefaultRole(roles,
-				storage.getEditableDashboardIds(userid)
-						.stream()
-						.map(Role::getDashboardEditorRole)
-						.collect(Collectors.toSet()),
-				Role.DASHBOARD_EDITOR);
-
 		return roles;
+	}
+
+	private static void addWithDefaultRole(Collection<Role> roles, Stream<Role> rolesToAdd, Role defaultRole) {
+		addWithDefaultRole(roles, rolesToAdd.collect(toSet()), defaultRole);
 	}
 
 	private static void addWithDefaultRole(Collection<Role> roles, Collection<Role> rolesToAdd, Role defaultRole) {
