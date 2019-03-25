c908117b037cb8e33c0c2eb7db9fdf1281c8e196
neumaol <neumaol@hlag.com>
2019-03-25 11:28:36 +0100

    dynamically adjust the number of allowed calls to jira

--- core/src/main/java/com/hlag/oversigt/connect/jira/LimitedJiraClient.java
+++ core/src/main/java/com/hlag/oversigt/connect/jira/LimitedJiraClient.java
@@ -7,9 +7,12 @@ import com.google.common.util.concurrent.RateLimiter;
 import com.hlag.oversigt.properties.Credentials;
 import com.hlag.oversigt.properties.ServerConnection;
 
+import io.atlassian.util.concurrent.atomic.AtomicInteger;
+
 public class LimitedJiraClient extends JiraClientFilter {
-	private static final int ALLOWED_CALLS_PER_SECOND = 1;
-	private static final RateLimiter RATE_LIMITER = RateLimiter.create(ALLOWED_CALLS_PER_SECOND);
+	private static final RateLimiter RATE_LIMITER = RateLimiter.create(1.0);
+
+	private static final AtomicInteger runningSearches = new AtomicInteger(0);
 
 	public LimitedJiraClient(final ServerConnection connection, final Credentials credentials)
 			throws JiraClientException {
@@ -17,16 +20,25 @@ public class LimitedJiraClient extends JiraClientFilter {
 	}
 
 	/**
-	 * Perform the Jira search but limit the number of calls to one per second
-	 * 
+	 * Perform the Jira search but limit the number of calls per second to the JIRA server. The
+	 * limit is dynamically adjusted with every incoming request
+	 *
 	 * @see com.hlag.oversigt.connect.jira.JiraClient#search(java.lang.String, int, int)
 	 */
 	@Override
 	public List<Issue> search(String jql, int maxResults, int startAt) throws JiraClientException {
+		// change the number of allowed searches
+		int limit = runningSearches.incrementAndGet();
+		RATE_LIMITER.setRate(limit);
+
 		// wait until we may proceed
 		RATE_LIMITER.acquire();
 
 		// let the JIRA client do its job
-		return getJiraClient().search(jql, maxResults, startAt);
+		try {
+			return getJiraClient().search(jql, maxResults, startAt);
+		} finally {
+			runningSearches.decrementAndGet();
+		}
 	}
 }
