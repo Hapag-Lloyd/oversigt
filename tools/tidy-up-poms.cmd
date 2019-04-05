4f3413c0b71b705ddae18df6ff49fbdebad0d829
Lars Knickrehm <mail@lars-sh.de>
2019-04-06 00:30:08 +0200

    Add tools

---
+++ tools/tidy-up-poms.cmd
@@ -0,0 +1,16 @@
+@echo off
+
+rem Remember current working directory to revert to it after processing
+set old_cd=%CD%
+
+rem Change directory to project root
+cd /d "%~dp0\.."
+
+rem Tidy up POMs
+call mvn org.codehaus.mojo:tidy-maven-plugin:pom
+
+rem Revert working directory and pause
+cd /d "%old_cd%"
+
+rem Keep results visible
+pause
