05c5b8d7f00084e04a975a216e535ba43ba4b892
Lars Knickrehm <mail@lars-sh.de>
2019-04-12 17:13:13 +0200

    Initialize file system manually

--- core/src/main/java/com/hlag/oversigt/core/OversigtServer.java
+++ core/src/main/java/com/hlag/oversigt/core/OversigtServer.java
@@ -13,17 +13,21 @@ import java.io.IOException;
 import java.io.StringWriter;
 import java.net.BindException;
 import java.net.InetSocketAddress;
+import java.net.URI;
 import java.net.URISyntaxException;
 import java.net.URL;
 import java.nio.ByteBuffer;
 import java.nio.charset.Charset;
 import java.nio.charset.StandardCharsets;
+import java.nio.file.FileSystems;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
 import java.util.Arrays;
 import java.util.Deque;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 import java.util.Objects;
 import java.util.Optional;
 import java.util.function.Consumer;
@@ -473,8 +477,16 @@ public class OversigtServer extends AbstractIdleService {
 	private HttpHandler createAngularHandler(final String prefix) {
 		final Path basePath;
 		try {
-			basePath = Paths.get(Resources.getResource(prefix).toURI());
-		} catch (final URISyntaxException e) {
+			final URI uri = Resources.getResource(prefix).toURI();
+
+			// Initialize file system
+			// This might be required for ZipFileSystem
+			final Map<String, String> env = new HashMap<>();
+			env.put("create", "true");
+			FileSystems.newFileSystem(uri, env);
+
+			basePath = Paths.get(uri);
+		} catch (final IOException | URISyntaxException e) {
 			throw new RuntimeException(String.format("Unable to find prefix '%s' in resources", prefix), e);
 		}
 		final Path indexHtml = basePath.resolve("index.html");
