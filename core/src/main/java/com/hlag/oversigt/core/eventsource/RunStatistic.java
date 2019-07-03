25419d6e149c1354334942bd30c2da6488ce4a65
Olaf Neumann <github@olafneumann.org>
2019-07-03 22:41:17 +0200

    fix checkstyle warnings

--- core/src/main/java/com/hlag/oversigt/core/eventsource/RunStatistic.java
+++ core/src/main/java/com/hlag/oversigt/core/eventsource/RunStatistic.java
@@ -7,7 +7,7 @@ import java.util.Collections;
 import java.util.List;
 import java.util.Optional;
 
-public class RunStatistic {
+public final class RunStatistic {
 	public static StatisticsCollector createCollector() {
 		return new StatisticsCollector();
 	}
