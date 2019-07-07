523f19ad4149bfef032734e030c30550fdc91dea
Lars Knickrehm <mail@lars-sh.de>
2019-07-08 01:19:03 +0200

    Update tool scripts

--- tools/display-plugin-updates.cmd
+++ tools/display-plugin-updates.cmd
@@ -7,7 +7,7 @@ rem Change directory to project root
 cd /d "%~dp0\.."
 
 rem Display plugin updates
-call mvn org.codehaus.mojo:versions-maven-plugin:display-plugin-updates
+call mvn org.codehaus.mojo:versions-maven-plugin:display-plugin-updates -Dmaven.version.rules="file:///%old_cd%/rules.xml"
 
 rem Revert working directory and pause
 cd /d "%old_cd%"
--- tools/update-dependencies.cmd
+++ tools/update-dependencies.cmd
@@ -6,11 +6,14 @@ set old_cd=%CD%
 rem Change directory to project root
 cd /d "%~dp0\.."
 
+rem Update parent version of child modules
+call mvn org.codehaus.mojo:versions-maven-plugin:update-child-modules -DgenerateBackupPoms=false -DallowSnapshots=true
+
 rem Update version properties
-call mvn org.codehaus.mojo:versions-maven-plugin:update-properties -DgenerateBackupPoms=false
+call mvn org.codehaus.mojo:versions-maven-plugin:update-properties -DgenerateBackupPoms=false -Dmaven.version.rules="file:///%old_cd%/rules.xml"
 
 rem Update version tags
-call mvn org.codehaus.mojo:versions-maven-plugin:use-latest-releases -DgenerateBackupPoms=false
+call mvn org.codehaus.mojo:versions-maven-plugin:use-latest-releases -DgenerateBackupPoms=false -Dmaven.version.rules="file:///%old_cd%/rules.xml"
 
 rem Revert working directory and pause
 cd /d "%old_cd%"
