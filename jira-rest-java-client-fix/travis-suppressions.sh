135f71abbea75479c1b1d417973e14ba370dffd8
Lars Knickrehm <mail@lars-sh.de>
2020-08-21 19:35:59 +0200

    Update Travis Suppressions

--- jira-rest-java-client-fix/travis-suppressions.sh
+++ jira-rest-java-client-fix/travis-suppressions.sh
@@ -1,10 +1,25 @@
-# Dependencies Report: The Dependencies Report fails resolving some Atlassian specific sub-dependencies.
-function suppressAtlassianDependenciesReport() {
-	cat < /dev/stdin \
-	| grep --invert-match --perl-regexp "^\\[INFO\\] Unable to create Maven project from repository for artifact 'com\\.atlassian\\..*', for more information run with -X$"
-	| grep --invert-match --perl-regexp "^\\[WARNING\\] Failed to build parent project for com\\.atlassian\\..*$"
-	| grep --invert-match --perl-regexp "^\\[WARNING\\] The POM for .*atlassian.* is missing, no dependency information available$"
+# JDK8: The Dependencies Report fails resolving some Atlassian specific sub-dependencies.
+function suppressJdk8AtlassianDependenciesReport() {
+	if [ ${JDK_VERSION} -eq 8 ]; then
+		cat < /dev/stdin \
+		| grep --invert-match --perl-regexp "^\\[INFO\\] Unable to create Maven project from repository for artifact 'com\\.atlassian\\..*', for more information run with -X$"
+		| grep --invert-match --perl-regexp "^\\[WARNING\\] Failed to build parent project for com\\.atlassian\\..*$"
+		| grep --invert-match --perl-regexp "^\\[WARNING\\] The POM for .*atlassian.* is missing, no dependency information available$"
+	else
+		cat < /dev/stdin
+	fi
+}
+
+# JDK11 and later: Inspecting try-with-resource fails as of https://github.com/spotbugs/spotbugs/issues/259
+function suppressJdk11AndLaterSpotbugsTryWithResourceNPE() {
+	if [ ${JDK_VERSION} -ge 11 ]; then
+		cat < /dev/stdin \
+		| grep --invert-match --perl-regexp "^TODO$"
+	else
+		cat < /dev/stdin
+	fi
 }
 
 cat < /dev/stdin \
-| suppressAtlassianDependenciesReport
+| suppressJdk11AndLaterSpotbugsTryWithResourceNPE
+| suppressJdk8AtlassianDependenciesReport
