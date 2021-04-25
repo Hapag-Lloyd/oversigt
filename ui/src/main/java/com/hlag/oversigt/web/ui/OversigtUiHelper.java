a0e9df63ae63b1b8e1376e2c2af3fa2058dbc8cf
Lars Knickrehm <mail@lars-sh.de>
2021-04-25 23:54:09 +0200

    Remove UI dependency to Guava

--- ui/src/main/java/com/hlag/oversigt/web/ui/OversigtUiHelper.java
+++ ui/src/main/java/com/hlag/oversigt/web/ui/OversigtUiHelper.java
@@ -1,10 +1,12 @@
 package com.hlag.oversigt.web.ui;
 
 import java.net.URI;
-import java.net.URISyntaxException;
+import java.nio.file.Path;
+import java.nio.file.Paths;
 import java.util.Optional;
 
-import com.google.common.io.Resources;
+import de.larssh.utils.Nullables;
+import de.larssh.utils.io.Resources;
 
 /**
  * Class containing information to get to the Oversigt UI.
@@ -24,10 +26,9 @@ public final class OversigtUiHelper {
 	 * @return the URI to the Oversigt UI if found - otherwise empty()
 	 */
 	public static Optional<URI> getPathToUiResources() {
-		try {
-			return Optional.of(Resources.getResource("oversigt-ui").toURI());
-		} catch (@SuppressWarnings("unused") final IllegalArgumentException | URISyntaxException ignore) {
-			return Optional.empty();
-		}
+		final ClassLoader classLoader = Nullables.orElseGet( //
+				OversigtUiHelper.class.getClassLoader(),
+				ClassLoader::getSystemClassLoader);
+		return Resources.getResource(classLoader, Paths.get("oversigt-ui")).map(Path::toUri);
 	}
 }
